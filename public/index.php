03f4144c23a85b98c650a056b61e70458a4105c2
Jil Oliveira <61645060349@tjce.jus.br>
2024-11-15 13:55:29 -0300

    feat: change index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,20 @@
 <?php
 
-echo "<h3>Api Padrão</h3>";
-echo "<p>Bem-vindo à API Padrão!</p>";
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+  $request = Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
