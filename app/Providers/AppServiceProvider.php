49c237d5451eee90281d447dfc6209f3c4731533
Jil Oliveira <61645060349@tjce.jus.br>
2024-11-17 16:35:00 -0300

    feat: remove scramble and sanctum

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,17 +4,16 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-use Illuminate\Support\Facades\Gate;
-
 
 class AppServiceProvider extends ServiceProvider
 {
-  public function register(): void {}
+  public function register(): void
+  {
+    //
+  }
 
   public function boot(): void
   {
-    // Gate::define('viewApiDocs', function () {
-    //   return config('app.env') === 'production';
-    // });
+    //
   }
 }
